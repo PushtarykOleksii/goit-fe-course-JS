72570f8c0f5d85358dc1f57daca523b40833289a
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-07-12 06:46:00 +0300

    going 1

--- module_1/task-4.js
+++ module_1/task-4.js
@@ -9,3 +9,20 @@
 // Проверяет сможет ли пользователь оплатить заказ:
 // если сумма к оплате превышает количество кредитов на счету, выводи в консоль сообщение 'Недостаточно средств на счету!'.
 // в противном случае необходимо посчитать остаток кредитов на счету и вывести сообщение 'Вы купили [число] дроидов, на счету осталось [число] кредитов.'.
+
+let credits = 23580;
+const pricePerDroid = 3000;
+let order = prompt(`How many Droids do you want to buy?`);
+let totalPrice = order * pricePerDroid;
+credits = credits - totalPrice;
+if (order === null) {
+  console.log(`'Declined by the User!'`);
+} else if (totalPrice > credits) {
+  console.log(
+    `Not enough credits on your account, total prise is ${totalPrice}!!!`
+  );
+} else if (totalPrice <= credits) {
+  console.log(
+    `You have bought ${order} Droids, you have left ${credits} credits on your account`
+  );
+}
--- module_1/task-5.js
+++ module_1/task-5.js
@@ -11,3 +11,20 @@
 // Австралия - 170 кредитов
 // Индия - 80 кредитов
 // Ямайка - 120 кредитов
+
+const China = 100;
+const Chili = 250;
+const Australia = 170;
+const India = 80;
+const Jamaica = 120;
+let message = `Dellivery to  `
+const promptLabel = `'Choose your country to deliver: China - ${China}, Chili-${Chili}, Australia-${Australia}, India-${India}, Jamaica-${Jamaica},`;
+const userChoise = prompt(promptLabel.toLowerCase());
+if (userChoise ===null) {
+    console.log(`'Declined by the User!'`);
+} else {
+    switch(userChoise) {
+        case China: 
+console.log();
+    }
+}

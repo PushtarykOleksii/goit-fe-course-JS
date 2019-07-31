f0d60b7ff5bdf65b2df59bdb85bbf6c12df8ede1
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-07-31 12:13:23 +0300

    finish

--- modul_10/src/index.js
+++ modul_10/src/index.js
@@ -1,33 +1,24 @@
 import "./css/styles.css";
 import menuData from "./menus.json";
 import menuTemplate from "./templates/menu.hbs";
-
-const Theme = {
-  LIGHT: "light-theme",
-  DARK: "dark-theme"
-};
 const list = document.querySelector(".menu");
 const body = document.querySelector("body");
 const checkInput = document.querySelector(".switch__input");
-
 const setBefor = localStorage.getItem("class");
-const setAfter = JSON.parse(setBefor);
-
-if (setAfter === "dark-theme") {
+if (setBefor === "dark-theme") {
   body.classList.add("dark-theme");
   checkInput.checked = true;
 }
 body.addEventListener("change", event => {
   if (checkInput.checked) {
     body.classList.add("dark-theme");
-    localStorage.setItem("class", JSON.stringify(Theme.DARK));
+    localStorage.setItem("class", "dark-theme");
   } else {
     body.classList.remove("dark-theme");
     localStorage.removeItem("class");
   }
 });
 buildMenu(menuData);
-
 function buildMenu(menuData) {
   const menu = menuData.map(item => menuTemplate(item)).join("");
   list.insertAdjacentHTML("beforeend", menu);
--- modul_10/webpack/configs/development.js
+++ modul_10/webpack/configs/development.js
@@ -24,7 +24,7 @@ module.exports = env => ({
     publicPath: '',
     historyApiFallback: true,
     compress: true,
-    port: 3000,
+    port: 3001,
     noInfo: true,
     quiet: true,
     clientLogLevel: 'warning',
